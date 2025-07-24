1c2d40723745b5ec3a6730777c0a2276e264eafc
Alex Dong <190415+alexdong@users.noreply.github.com>
2025-07-25 09:32:07 +1200

    feat: add sitecustomize.py for default encoding and enhanced error handling in development

---
+++ sitecustomize.py
@@ -0,0 +1,12 @@
+# Set default encoding
+import locale
+import os
+import sys
+
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+
+# Enable better error messages in development
+if os.environ.get("DEVELOPMENT"):
+    import pdb
+
+    sys.excepthook = lambda *args: pdb.post_mortem()  # noqa: ARG005
