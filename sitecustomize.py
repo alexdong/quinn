21cc9d087e844791856d80aaa4453cbf33846586
Alex Dong <190415+alexdong@users.noreply.github.com>
2025-07-25 14:24:30 +1200

    fix: update environment variable for enabling debug error messages

--- sitecustomize.py
+++ sitecustomize.py
@@ -6,7 +6,7 @@ import sys
 locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
 # Enable better error messages in development
-if os.environ.get("DEVELOPMENT"):
+if os.environ.get("DEBUG"):
     import pdb
 
     sys.excepthook = lambda *args: pdb.post_mortem()  # noqa: ARG005
